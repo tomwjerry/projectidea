d47b946934274f4f409d6d35425f504802666af9
Tom <tom@jatech.se>
2023-04-14 15:27:14 +0200

    Smalled required migration

---
+++ app/Models/StoryLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StoryLog extends Model
+{
+    use HasFactory;
+}
